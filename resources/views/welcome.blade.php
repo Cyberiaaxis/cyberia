6a36b6104c363ca4a7f1392dd86524960c9434c1
Cyberia <norepply@cyberiaaxis.com>
2020-03-04 16:03:32 +0000

    welcomepage work on progress

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -149,21 +149,41 @@
 
                     <div class="modal-body">
 
-                        <div class="form-group">
+                <div class="signin-content">
+                    <div class="signin-image m-auto">
+                        <figure><i class="fa fa-2x fa-sign-in"></i></figure>
+                        <a href="#" class="signup-image-link">Create an account</a>
+                    </div>
 
-                            <label for="name" class="col-form-label text-white">User Name:</label>
-
-                            <input type="text" class="form-control" name="username" required autofocus>
-
+                    <div class="signin-form">
+                        <h2 class="form-title">Sign up</h2>
+                        <form method="POST" class="register-form" id="login-form">
+                            <div class="form-group">
+                                <label for="your_name"><i class="zmdi zmdi-account material-icons-name"></i></label>
+                                <input type="text" name="your_name" id="your_name" placeholder="Your Name"/>
+                            </div>
+                            <div class="form-group">
+                                <label for="your_pass"><i class="zmdi zmdi-lock"></i></label>
+                                <input type="password" name="your_pass" id="your_pass" placeholder="Password"/>
+                            </div>
+                            <div class="form-group">
+                                <input type="checkbox" name="remember-me" id="remember-me" class="agree-term" />
+                                <label for="remember-me" class="label-agree-term"><span><span></span></span>Remember me</label>
+                            </div>
+                            <div class="form-group form-button">
+                                <input type="submit" name="signin" id="signin" class="form-submit" value="Log in"/>
+                            </div>
+                        </form>
+                        <div class="social-login">
+                            <span class="social-label">Or login with</span>
+                            <ul class="socials">
+                                <li><a href="#"><i class="display-flex-center zmdi zmdi-facebook"></i></a></li>
+                                <li><a href="#"><i class="display-flex-center zmdi zmdi-twitter"></i></a></li>
+                                <li><a href="#"><i class="display-flex-center zmdi zmdi-google"></i></a></li>
+                            </ul>
                         </div>
-
-                        <div class="form-group">
-
-                            <label for="password" class="col-form-label text-white">Password:</label>
-
-                            <input type="password" class="form-control" name="password" required autofocus>
-
-                        </div>
+                    </div>
+                </div>
 
                     </div>
 
