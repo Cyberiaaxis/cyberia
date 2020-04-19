5aa99fde118c806311960865662d1a3fa2745472
Cyberiaaaxis <10651522+cyberiaaxis@users.noreply.github.com>
2020-04-20 00:41:04 +0530

    added new ffiles

---
+++ database/factories/MedalFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Model\Medal;
+use Faker\Generator as Faker;
+
+$factory->define(Medal::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
