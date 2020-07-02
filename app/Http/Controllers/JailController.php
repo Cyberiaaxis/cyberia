00c72b5fa5d1c17b08f7cdb5f9c4594d74106ce4
cyberiaaxis <cyberiaaxis@gmail.com>
2020-07-03 02:09:33 +0530

    jail hospital market code work start

---
+++ app/Http/Controllers/JailController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Model\Jail;
+use Illuminate\Http\Request;
+
+class JailController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Model\Jail  $jail
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Jail $jail)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Model\Jail  $jail
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Jail $jail)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Model\Jail  $jail
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Jail $jail)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Model\Jail  $jail
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Jail $jail)
+    {
+        //
+    }
+}
