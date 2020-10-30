170bb0a21ca6b97f5afbe910b16cf679ea701667
Cyberiaaaxis <10651522+cyberiaaxis@users.noreply.github.com>
2020-10-30 22:41:10 +0530

    Revert "here was changes"
    This reverts commit 4f59315e20468bc5f78b89425452c593cb76bc1a.

--- theme/src/components/Navbar.js
+++ theme/src/components/Navbar.js
@@ -29,9 +29,7 @@ export default function Navbar() {
             <LoginForm />
             <Modal body='Home' id='home'/>
             <Modal body='Screenshot' id='screens' />
-
             <Modal body='About Us' id='aboutus' />
-
             <Modal body='Contact Us' id='contactus' />
         </div>
     </>)
