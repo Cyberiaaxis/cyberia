6cc37bad603a3ded72d12341eb17a1d1241d9a4c
Cyberiaaaxis <10651522+cyberiaaxis@users.noreply.github.com>
2020-10-30 22:54:11 +0530

    herherherherehrehre

--- theme/src/components/Navbar.js
+++ theme/src/components/Navbar.js
@@ -28,7 +28,6 @@ export default function Navbar() {
             </ul>
             <LoginForm />
             <Modal body='Home' id='home'/>
-            asdfsghdsfghjkugftds
             <Modal body='Screenshot' id='screens' />
             <Modal body='About Us' id='aboutus' />
             <Modal body='Contact Us' id='contactus' />
