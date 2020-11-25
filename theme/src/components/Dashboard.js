376d6dc5379ee5b734c1c3e3f7c004bc587e5725
Cyberiaaaxis <10651522+cyberiaaxis@users.noreply.github.com>
2020-11-26 01:41:08 +0530

    dashboard improvement

--- theme/src/components/Dashboard.js
+++ theme/src/components/Dashboard.js
@@ -1,22 +1,96 @@
 import React, { useState } from "react";
 import '../styles/Dashboard.scss';
 
-const pages = [
+const topBar = [
     {
-        "url": "home",
-        "name": "playerHome"
+        "name": "Home",
+        "url": "playerHome",
+        "subMenu": [
+        {
+            "name": "Home1",
+            "url": "playerHome",
+                    "subMenu": null
+        },
+        {
+            "name": "Home2",
+            "url": "playerHome",
+                    "subMenu": null
+        },
+        {
+            "name": "Home3",
+            "url": "playerHome",
+                    "subMenu": null
+        },
+    ],
     },
     {
-        "name": "playerExplore",
-        "url": "explore"
+        "name": "Actions",
+        "url": "playerActions",
+        "subMenu": null
     },
     {
-        "name": "citye",
-        "url": "city"
+        "name": "Travel",
+        "url": "PlayerTravel",
+        "subMenu": null
     },
     {
-        "name": "market",
-        "url": "market"
+        "name": "Communication",
+        "url": "playerCommunication",
+        "subMenu": null
+    }
+];
+
+const bottomBar = [
+    {
+        "name": "Settings",
+        "url": "playerSettings",
+        "sub-menu": null
+    },
+    {
+        "name": "Analyst",
+        "url": "playerAnalyst",
+                "sub-menu": null
+    },
+    {
+        "name": "Achievement",
+        "url": "playerAchievement",
+                "sub-menu": null
+    },
+    {
+        "name": "Competition",
+        "url": "playerCompetition",
+                "sub-menu": null
+    }
+];
+
+const leftSideBar = [
+    {
+        "name": "Start",
+        "url": "playerExplore",
+                "sub-menu": null
+    },
+    {
+        "name": "Inventory",
+        "url": "PlayerInventory",
+                "sub-menu": null
+    },
+    {
+        "name": "Donator House",
+        "url": "city",
+                "sub-menu": null
+    }
+];
+
+const RightSideBar = [
+    {
+        "name": "Competitions",
+        "url": "competitions",
+                "sub-menu": null
+    },
+    {
+        "name": "Hall of Fame",
+        "url": "hof",
+                "sub-menu": null
     }
 ];
 
@@ -29,14 +103,30 @@ const Header = ({ section, setSection }) => {
     }
 
     return (
+
         <header>
-            <nav className="navbar">
+            <ul className="navbar">
                 {
-                    pages.map(function (value, key) {
-                        return <a className={section === value.url ? 'nav-link active' : 'nav-link'} onClick={handleClick} href="#" data-href={value.url}>{value.name}</a>
+                    topBar.map((menuTitle, key) => {
+
+                        let subMenuList = [];
+                        if (menuTitle.subMenu) {
+                            menuTitle.subMenu.map(subMenu => {
+                                // console.log(subMenu.name);
+                                subMenuList.push(<li><a className={section === subMenu.url ? 'menuList nav-link active' : 'menuList nav-link'} key={key} href="#" data-href={subMenu.url}>{subMenu.name}</a></li>);
+                                // console.log(subMenuList);
+                            })
+                        }
+
+                        return (
+                            <li>
+                                <a className={section === menuTitle.url ? 'menuList nav-link active' : 'menuList nav-link'} key={key} href="#" data-href={menuTitle.url}>{menuTitle.name}</a>
+                                <ul className="dropdown-menu">{subMenuList}</ul>
+                            </li>
+                        )
                     })
                 }
-            </nav>
+            </ul>
         </header>
     );
 };
@@ -52,7 +142,7 @@ const Footer = ({ section, setSection }) => {
         <footer>
             <nav className="navbar">
                 {
-                    pages.map(function (value, key) {
+                    bottomBar.map(function (value, key) {
                         return <a className={section === value.url ? 'nav-link active' : 'nav-link'} onClick={handleClick} href="#" data-href={value.url}>{value.name}</a>
                     })
                 }
@@ -72,7 +162,7 @@ const AsideLeft = ({ section, setSection }) => {
         <aside>
             <nav className='sidebar-left'>
                 {
-                    pages.map(function (value, key) {
+                    leftSideBar.map(function (value, key) {
                         return <a className={section === value.url ? 'nav-link active' : 'nav-link'} onClick={handleClick} href="#" data-href={value.url}>{value.name}</a>
                     })
                 }
@@ -93,7 +183,7 @@ const AsideRight = ({ section, setSection }) => {
         <aside>
             <nav  className='sidebar-right'>
                 {
-                    pages.map(function (value, key) {
+                    RightSideBar.map(function (value, key) {
                         return <a className={section === value.url ? 'nav-link active' : 'nav-link'} onClick={handleClick} href="#" data-href={value.url}>{value.name}</a>
                     })
                 }
@@ -138,10 +228,10 @@ export default function Dashboard() {
         <div className="dashboard">
             <div className="grid">
                 <Header {...{ section, setSection }} />
-                <AsideLeft {...{ section, setSection }} />
-                <Main section={section} />
-                <AsideRight {...{ section, setSection }} />
-                <Footer {...{ section, setSection }} />
+                {/* <AsideLeft {...{ section, setSection }} /> */}
+                {/* <Main section={section} /> */}
+                {/* <AsideRight {...{ section, setSection }} /> */}
+                {/* <Footer {...{ section, setSection }} /> */}
             </div>
         </div>
     );
