2ada3ed69bcd87f36f8100392ac0a033e427f7b9
Cyberiaaaxis <10651522+cyberiaaxis@users.noreply.github.com>
2020-12-28 00:06:40 +0530

    stats popover started

--- theme/src/components/Stats.js
+++ theme/src/components/Stats.js
@@ -1,25 +1,17 @@
 import React, { useState } from "react";
-import { Box, makeStyles, withStyles, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper } from "@material-ui/core/";
+import { Popover, Typography, Box, makeStyles, withStyles, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper } from "@material-ui/core/";
+import { CanvasJSChart } from "canvasjs-react-charts";
 
 const StyledTableCell = withStyles((theme) => ({
-
     head: {
-        color: '#3B7903',
-                    fontFamily: [
-                  "Alegreya SC",
-                  "serif"
-    ].join(','),
-    fontSize: 20,
-
+        color: "#3B7903",
+        fontFamily: ["Alegreya SC", "serif"].join(","),
+        fontSize: 20,
     },
     body: {
-                color: '#5F4821',
-                    fontFamily: [
-                  "Alegreya SC",
-                  "serif"
-    ].join(','),
-    fontSize: 17,
-
+        color: "#5F4821",
+        fontFamily: ["Alegreya SC", "serif"].join(","),
+        fontSize: 17,
     },
 }))(TableCell);
 
@@ -38,12 +30,52 @@ const rows = [createData("Won", 159, 6.0, 24, 4.0), createData("Lost", 237, 9.0,
 
 const useStyles = makeStyles({
     table: {
-                // background: "transparent",
+        // background: "transparent",
+    },
+    popover: {
+        pointerEvents: "none",
     },
 });
 
 export default function Stats() {
     const classes = useStyles();
+    const [anchorEl, setAnchorEl] = useState(null);
+
+    const options = {
+        exportEnabled: true,
+        animationEnabled: true,
+        title: {
+            text: "Website Traffic Sources",
+        },
+        data: [
+            {
+                type: "pie",
+                startAngle: 75,
+                toolTipContent: "<b>{label}</b>: {y}%",
+                showInLegend: "true",
+                legendText: "{label}",
+                indexLabelFontSize: 16,
+                indexLabel: "{label} - {y}%",
+                dataPoints: [
+                    { y: 18, label: "Direct" },
+                    { y: 49, label: "Organic Search" },
+                    { y: 9, label: "Paid Search" },
+                    { y: 5, label: "Referral" },
+                    { y: 19, label: "Social" },
+                ],
+            },
+        ],
+    };
+
+    const handlePopoverOpen = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+
+    const handlePopoverClose = () => {
+        setAnchorEl(null);
+    };
+
+    const open = Boolean(anchorEl);
 
     return (
         <>
@@ -53,7 +85,60 @@ export default function Stats() {
                         <Table className={classes.table} aria-label="customized table">
                             <TableHead>
                                 <TableRow>
-                                    <StyledTableCell>Battle Stats</StyledTableCell>
+                                    <Typography className={classes.head} aria-owns={open ? "mouse-over-popover" : undefined} aria-haspopup="true" onMouseEnter={handlePopoverOpen} onMouseLeave={handlePopoverClose}>
+                                        <StyledTableCell>Working Stats</StyledTableCell>
+                                    </Typography>
+                                    <Popover
+                                        id="mouse-over-popover"
+                                        className={classes.popover}
+                                        classes={{
+                                            paper: classes.paper,
+                                        }}
+                                        open={open}
+                                        anchorEl={anchorEl}
+                                        anchorOrigin={{
+                                            vertical: "bottom",
+                                            horizontal: "left",
+                                        }}
+                                        transformOrigin={{
+                                            vertical: "top",
+                                            horizontal: "left",
+                                        }}
+                                        PaperProps={{
+                                            style: { width: "100%" },
+                                        }}
+                                        onClose={handlePopoverClose}
+                                        disableRestoreFocus
+                                    >
+                                        <TableContainer>
+                                            <Table className={classes.table} aria-label="customized table">
+                                                <TableHead>
+                                                    <TableRow>
+                                                        <StyledTableCell>Criminal Recode</StyledTableCell>
+                                                        <StyledTableCell> Total</StyledTableCell>
+                                                        <StyledTableCell align="right">Status</StyledTableCell>
+                                                    </TableRow>
+                                                </TableHead>
+                                                <TableBody>
+                                                    {rows.map((row) => (
+                                                        <StyledTableRow key={row.name}>
+                                                            <StyledTableCell component="th" scope="row">
+                                                                {row.name}
+                                                            </StyledTableCell>
+                                                            <StyledTableCell align="right">{row.calories}</StyledTableCell>
+                                                            <StyledTableCell component="th" scope="row" height='100' width='300'>
+                                                            </StyledTableCell>
+                                                        </StyledTableRow>
+                                                    ))}
+                                                                                                                    <CanvasJSChart
+                                                                    options={options}
+                                                                    /* onRef={ref => this.chart = ref} */
+                                                                />
+
+                                                </TableBody>
+                                            </Table>
+                                        </TableContainer>
+                                    </Popover>
                                     <StyledTableCell align="right">Total</StyledTableCell>
                                 </TableRow>
                             </TableHead>
@@ -90,7 +175,6 @@ export default function Stats() {
                             </TableBody>
                         </Table>
                     </TableContainer>
-
                 </Box>
                 <Box display="flex">
                     <TableContainer>
@@ -134,11 +218,8 @@ export default function Stats() {
                             </TableBody>
                         </Table>
                     </TableContainer>
-
                 </Box>
-
             </Box>
-
         </>
     );
 }
